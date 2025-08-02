a80468dc75530527c4c249430d1163064f7ac6ce
Martin Moene <martin.moene@gmail.com>
2025-08-02 13:10:54 +0200

    Add example/01-basic.cpp

---
+++ example/01-basic.cpp
@@ -0,0 +1,56 @@
+// Use nonstd::string's split():
+
+#include "nonstd/string.hpp"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#if string_CPP11_000
+
+template< typename T >
+std::string contents(std::vector<T> const & coll)
+{
+    // using to_string() for nonstd::string::string_view:
+
+    std::stringstream os;
+    for ( auto const & elem : coll )
+        os << "'" << std::string(elem) << "', ";
+    return os.str();
+}
+
+#else
+
+template< typename T >
+std::string contents(std::vector<T> const & coll)
+{
+    // using to_string() for nonstd::string::string_view:
+
+    typename std::vector<T>::const_iterator pos = coll.begin();
+    typename std::vector<T>::const_iterator end = coll.end();
+
+    std::stringstream os;
+    for ( ; pos != end; ++pos )
+        os << "'" << std::string(*pos) << "', ";
+    return os.str();
+}
+
+#endif
+
+template< typename T >
+std::ostream & operator<<(std::ostream & os, std::vector<T> const & coll )
+{
+    return os << "[" << contents(coll) << "]";
+}
+
+int main()
+{
+    std::cout << nonstd::split("Hello, world", ",");
+}
+
+// cl -nologo -EHsc -I../include 01-basic.cpp && 01-basic.exe
+// clang-cl -EHsc -D_CRT_SECURE_NO_WARNINGS -I../include 01-basic.cpp && 01-basic.exe
+// g++ -std=c++98 -Wall -I../include -o 01-basic.exe 01-basic.cpp && 01-basic.exe
+
+// Output:
+// ['Hello', ' world', ]
