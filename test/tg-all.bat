bb3b95451e9ff648fcc838d6d5d02dd8c6f1985c
Martin Moene <martin.moene@gmail.com>
2025-08-02 13:09:41 +0200

    Update tg-all.bat

--- test/tg-all.bat
+++ test/tg-all.bat
@@ -1,14 +1,6 @@
 :: g++ - Compile all permutations of C++ std and string_view-s:
 
 @echo off
-for %%v in ( string_CONFIG_SELECT_STRING_VIEW_INTERNAL string_CONFIG_SELECT_STRING_VIEW_NONSTD ) do (
-    for %%s in ( c++98 c++03 c++11 c++14 c++17 c++2a ) do (
-        call tg.bat %%s %%v
-    )
-)
-
-for %%v in ( string_CONFIG_SELECT_STRING_VIEW_STD ) do (
-    for %%s in ( c++17 c++2a ) do (
-        call tg.bat %%s %%v
-    )
+for %%s in ( c++11 c++14 c++17 c++20 c++23 ) do (
+    call tg.bat %%s
 )
